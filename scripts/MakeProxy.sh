9cc577dc218d981f11a0129def25cb6d1fcde453
Javier <72555618+JaviBot00@users.noreply.github.com>
2021-11-24 01:48:46 +0100

    Update MakeProxy.sh

--- scripts/MakeProxy.sh
+++ scripts/MakeProxy.sh
@@ -4,5 +4,5 @@
 
 echo "Creando el HAProxy"
 
-docker run -d --name=pstream --restart always --network=my-net --ip=172.20.0.100 -p 1935:5000 -v /root/PFC/configuracion/pstream/nginx.conf:/etc/nginx/nginx.conf nginx:latest
-docker run -d --name=pweb --restart always --network=my-net --ip=172.20.0.101 -p 8888:5000 -v /root/PFC/configuracion/pweb/nginx.conf:/etc/nginx/nginx.conf nginx:latest
+docker run -d --name=pstream --restart always --network=my-net --ip=172.20.0.100 -p 1935:1935 -v /root/PFC/configuracion/pstream/nginx.conf:/etc/nginx/nginx.conf nginx:latest
+docker run -d --name=pweb --restart always --network=my-net --ip=172.20.0.101 -p 8888:8080 -v /root/PFC/configuracion/pweb/nginx.conf:/etc/nginx/nginx.conf nginx:latest
