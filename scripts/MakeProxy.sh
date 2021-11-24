3a59f7b6c795dc3e41902e39448f296b8280c44d
Javier <72555618+JaviBot00@users.noreply.github.com>
2021-11-24 03:09:48 +0100

    Delete scripts directory

--- scripts/MakeProxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Creacion del HAProxy
-
-echo "Creando el HAProxy"
-
-docker run -d --name=pstream --restart always --network=my-net --ip=172.20.0.100 -p 1935:1935 -v /root/PFC/configuracion/pstream/nginx.conf:/etc/nginx/nginx.conf nginx:latest
-docker run -d --name=pweb --restart always --network=my-net --ip=172.20.0.101 -p 8888:8080 -v /root/PFC/configuracion/pweb/nginx.conf:/etc/nginx/nginx.conf nginx:latest
