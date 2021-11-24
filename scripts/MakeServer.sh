909ec6ae282cbac39881cd0784efaa3642b282a0
Javier <72555618+JaviBot00@users.noreply.github.com>
2021-11-24 03:23:57 +0100

    Update MakeServer.sh

--- scripts/MakeServer.sh
+++ scripts/MakeServer.sh
@@ -16,8 +16,8 @@ sed -i '44,$d'  $confweb
 #Crear Server
 for ((i = 1 ; i <= $nserver ; i++)); do
   docker container run -d --name=server$i --network=my-net --ip=172.20.0.$i -v /root/PFC/fuente/:/mnt/ -v /root/PFC/:/usr/local/nginx/html/ alqutami/rtmp-hls:latest
-  echo -e "        server 172.20.0.$i:1935" >> $confstream
-  echo -e "        server 172.20.0.$i:8080" >> $confweb
+  echo -e "        server 172.20.0.$i:1935;" >> $confstream
+  echo -e "        server 172.20.0.$i:8080;" >> $confweb
   echo $nIP $i
 done
 
