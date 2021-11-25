9bde02b4764b23f918fb03b4aecbbaf016e44750
Javier <72555618+JaviBot00@users.noreply.github.com>
2021-11-25 01:07:50 +0100

    Update DeleteServer.sh

--- scripts/DeleteServer.sh
+++ scripts/DeleteServer.sh
@@ -5,11 +5,13 @@ nserver=$1
 
 #Borrar Server
 for ((i = 1 ; i <= $nserver ; i++)); do
+  echo "Borrando Servidor de Stream" $i
   docker container stop server$i
   docker container rm server$i
 done
 
 #Borrando Nginx Proxy Inverso
+echo "Borrando Nginx Proxy Inverso"
 docker container stop pstream
 docker container rm pstream
 
@@ -17,7 +19,9 @@ docker container stop pweb
 docker container rm pweb
 
 #Borra Volumenes no Usados
+echo "Borra Volumenes no Usados"
 docker volume prune -f
 
 #Borra la Red Creada
+echo "Borra la Red Creada"
 docker network prune -f
