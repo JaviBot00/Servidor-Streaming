bd67c9df74cfa1a6967d3259516486aba20c0082
Javier <72555618+JaviBot00@users.noreply.github.com>
2021-11-25 02:36:29 +0100

    Update MakeProxy.sh

--- scripts/MakeProxy.sh
+++ scripts/MakeProxy.sh
@@ -2,8 +2,8 @@
 
 #Creacion del Proxy Nginx
 echo "Creacion del Proxy Nginx"
-echo "Proxy parte Stream"
 
+echo "Proxy parte Stream"
 docker run -d --name=pstream --restart always --network=my-net --ip=172.20.0.100 -p 1935:1935 -v /root/PFC/configuracion/pstream/nginx.conf:/etc/nginx/nginx.conf nginx:latest
 
 echo "Proxy parte Web"
