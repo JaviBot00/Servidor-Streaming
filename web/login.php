737c83ea533497b31c7f0a4a649bb9af823d41f6
Javier <72555618+JaviBot00@users.noreply.github.com>
2021-12-09 00:36:38 +0100

    Add files via upload

---
+++ web/login.php
@@ -0,0 +1,70 @@
+<?php
+
+// Credenciales de prueba
+$user = "admin";
+$pass = "qwerty";
+
+// Datos de acceso al servidor LDAP
+$host = "192.168.2.222";
+$port = "389";
+
+// Conexto donde se encuentran los usuarios
+$basedn = "cn=usuarios,ou=grupos,dc=botellamunoz,dc=com";
+
+// Atributos a recuperar
+$searchAttr = array("dn", "cn", "sn", "givenName");
+
+// Atributo para incorporar en la respuesta
+$displayAttr = "cn";
+
+// Respuesta por defecto
+$status = 1;
+$msg = "";
+$userDisplayName = "null";
+
+// Recuperar datos del POST
+if (isset($_POST['user'])) {
+        $user = $_POST['user'];
+}
+if (isset($_POST['pass'])) {
+        $pass = $_POST['pass'];
+}
+
+// Establecer la conexión con el servidor LDAP
+$ad = ldap_connect("ldap://{$host}:{$port}") or die("No se pudo conectar al servidor LDAP.");
+
+// Autenticar contra el servidor LDAP
+ldap_set_option($ad, LDAP_OPT_PROTOCOL_VERSION, 3);
+ldap_set_option($ad, LDAP_OPT_REFERRALS, 0);
+
+if (@ldap_bind($ad, "uid={$user},{$basedn}", $pass)) {
+        // En caso de éxito, recuperar los datos del usuario
+        $result = ldap_search($ad, $basedn, "(uid={$user})", $searchAttr);
+        $entries = ldap_get_entries($ad, $result);
+        if ($entries["count"]>0) {
+                // Si hay resultados en la búsqueda
+                $status = 0;
+                if (isset($entries[0][$displayAttr])) {
+                        // Recuperar el atributo a incorporar en la respuesta
+                        $userDisplayName = $entries[0][$displayAttr][0];
+                        $msg = "Autenticado como {$userDisplayName}";
+                }
+                else {
+                        // Si el atributo no está definido para el usuario
+                        $userDisplayName = "-";
+                        $msg = "Atributo no disponible ({$displayAttr})";
+                }
+        }
+        else {
+                // Si no hay resultados en la búsqueda, retornar error
+                $msg = "Error desconocido";
+        }
+}
+else {
+        // Si falla la autenticación, retornar error
+        $msg = "Usuario y/o contraseña inválidos";
+}
+
+// Respuesta en formato JSON
+header('Content-Type: application/json');
+echo "{\"uid\": \"{$user}\", \"estado\": \"{$status}\", \"nombre\": \"{$userDisplayName}\", \"debug\": \"{$msg}\"}";
---
+++ web/registrar.php
@@ -0,0 +1,53 @@
+<?php
+//Datos Usuario
+$name =  $_POST['Nombre'];
+$passwd = $_POST['Contraseña'];
+$uidnumber = strlen($name)+strlen($passwd)+rand(10,1000);
+
+// Datos de acceso al servidor LDAP
+$host = "192.168.2.222";
+$port = "389";
+$dnbind = "cn=admin,dc=botellamunoz,dc=com";
+$password = "qwerty";
+
+// Conexto donde se encuentran los usuarios
+$basedn = "uid=$name,cn=usuarios,ou=grupos,dc=botellamunoz,dc=com";
+
+// Establecer la conexión con el servidor LDAP
+$ds = ldap_connect("ldap://{$host}:{$port}") or die("No se pudo conectar al servidor LDAP.");
+if ($ds) {
+    ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3); // IMPORTANT
+    $result = ldap_bind($ds, $dnbind, $password); //BIND
+    if (!$result)
+    {
+    echo "Not Binded";
+    }
+
+    // Prépareles données
+    $ldaprecord['objectClass'][0] = "inetOrgPerson";
+    $ldaprecord['objectClass'][1] = "posixAccount";
+    $ldaprecord['objectClass'][2] = "shadowAccount";
+    $ldaprecord['uid'] = $name;
+    $ldaprecord['cn'] = $name;
+    $ldaprecord['sn'] = "usuario";
+    $ldaprecord['givenName'] = $name;
+    $ldaprecord['userPassword'] = $passwd;
+    $ldaprecord['uidnumber'] = $uidnumber;
+    $ldaprecord['gidNumber'] = "5001";
+    $ldaprecord['displayName'] = $name;
+    $ldaprecord['loginshell'] = "/bin/bash";
+    $ldaprecord['homedirectory'] = "/home/recorder/";
+
+    $r = ldap_add($ds, $basedn, $ldaprecord);
+   if ($r)
+   {
+   echo "Success";
+   }
+   else
+   {
+   echo ldap_errno($ds) ;
+   }
+} else {
+    echo "cannot connect to LDAP server at $AD_server.";
+}
+?>
