3f812b3dea4c50ebb987a8f5d9753ea262f1449b
Javier <72555618+JaviBot00@users.noreply.github.com>
2021-12-11 21:20:48 +0100

    Update login.php

--- web/login.php
+++ web/login.php
@@ -14,8 +14,9 @@ ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
 ldap_set_option($ds, LDAP_OPT_NETWORK_TIMEOUT, 10);
 
 $dn="uid=".$username.",".$ldapconfig['usersdn'].",".$ldapconfig['basedn'];
+$bind=ldap_bind($ds, $dn, $pass);
 if(isset($_POST['user'])){
-if ($bind=ldap_bind($ds, $dn, $pass)) {
+if ($bind) {
   echo("Login correct");//REPLACE THIS WITH THE CORRECT FUNCTION LIKE A REDIRECT;
 } else {
 
