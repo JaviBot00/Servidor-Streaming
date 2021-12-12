c0c5f5d1e9622890d90f48a3f71128318d9ba568
Javier <72555618+JaviBot00@users.noreply.github.com>
2021-12-12 20:32:18 +0100

    Add files via upload

--- web/php/playlist.php
+++ web/php/playlist.php
@@ -1,5 +1,4 @@
-<?php
-echo '<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="es">
 <head>
     <title>Listar Archivos</title>
@@ -29,38 +28,22 @@ echo '<!DOCTYPE html>
         <video controls width="700px">
             <source src="video1.mkv" type="video/mkv">
         </video>
-        <figcaption>'
-//Creamos Nuestra Función
-function lista_archivos($carpeta) { //La función recibira como parametro un carpeta
-    $carpeta='/home/recorder/grabaciones/';
-    if (is_dir($carpeta)) { //Comprovamos que sea un carpeta Valido
-       if ($dir = opendir($carpeta)) {//Abrimos el carpeta
-           echo '<ul>';
-           while (($archivo = readdir($dir)) !== false) { //Comenzamos a leer archivo por archivo
-               if ($archivo != '.' && $archivo != '..') {
-                   $nuevaRuta = $carpeta.$archivo.'/';
-                   echo '<li>'; //Abrimos un elemento de lista
-                   if (is_dir($nuevaRuta)) { //Si la ruta que creamos es un carpeta entonces:
-                       echo '<b>'.$nuevaRuta.'</b>'; //Imprimimos la ruta completa resaltandola en negrita
-                       lista_archivos($nuevaRuta);//Volvemos a llamar a este metodo para que explore ese carpeta.
-                   } else { //si no es un carpeta:
-//                     echo '<b>'.$nuevaRuta.'</b>'; //Imprimimos la ruta completa resaltandola en negrita
-                       echo "<li><a href=$archivo>$archivo</a></li>" //simplemente imprimimos el nombre del archivo actual
-                   }
-                   '</li>'; //Cerramos el item actual y se inicia la llamada al siguiente archivo
-                }
-            }//finaliza
-            echo '</ul>';//Se cierra la lista
-            closedir($dir);//Se cierra el archivo
-        }
-    } else {//Finaliza el If de la linea 12, si no es un carpeta valido, muestra el siguiente mensaje
-        echo 'No Existe la carpeta';
-    }
-}//Fin de la Función
-//Llamamos a la función
-lista_archivos("./ARCHIVOS/");
-echo '      </figcaption>
+        <figcaption>
+            <ul>
+                <?php
+                $directorio = "/home/recorder/grabaciones/";
+                #carpeta con archivos
+                $contador = 0;
+
+                $archivos = glob("$directorio/*");
+                introducir el código aquí
+
+                foreach ($archivos as $archivo) {   
+                     echo "<li><a href=/home/recorder/grabaciones/$archivo>$archivo</a></li>";
+                    }
+                ?>                
+            </ul>
+        </figcaption>
     </figure>
 </body>
-</html>'
-?>
+</html>
